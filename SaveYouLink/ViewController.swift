f6c77395201da36c169bc19d4ec449591a31f498
Nikita Perezhogin <51512651+Rayhard@users.noreply.github.com>
2020-08-18 20:40:18 +0300

    Initial Commit

---
+++ SaveYouLink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveYouLink
+//
+//  Created by Никита Пережогин on 18.08.2020.
+//  Copyright © 2020 Nikita Perezhogin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
