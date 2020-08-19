478c495555acff145e311c2e238e852b9f47380f
Nikita Perezhogin <51512651+Rayhard@users.noreply.github.com>
2020-08-19 11:09:12 +0300

    CoreData prototype

--- SaveYouLink/Controller/AllLinksViewController.swift
+++ SaveYouLink/Controller/AllLinksViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class AllLinksViewController: UIViewController {
 
@@ -14,12 +15,86 @@ class AllLinksViewController: UIViewController {
     
     
     @IBAction func addButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "New Link",
+                                                message: "Add new link",
+                                                preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) { action in
+                                        let tf = alertController.textFields?.first
+                                        if let newLink = tf?.text {
+                                            self.saveData(withLink: newLink)
+                                            self.tableView.reloadData()
+                                        }
+        }
+        
+        alertController.addTextField(configurationHandler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in }
+        
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func saveData(withLink link: String){
+        let context = getContext()
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "LinkItem", in: context) else { return }
+        
+        let linkObject = LinkItem(entity: entity, insertInto: context)
+//        linkObject.title = "test"
+        linkObject.urlLink = link
+        
+        do {
+            try context.save()
+            linkItems.append(linkObject)
+            
+        } catch let error as NSError{
+            print(error.localizedDescription)
+        }
     }
     
     var linkItems: [LinkItem] = []
     
+    // Получаем контекст
+    private func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // получаем данные из core data
+        let context = getContext()
+        let fetchRequest: NSFetchRequest<LinkItem> = LinkItem.fetchRequest()
+        
+        do {
+            linkItems = try context.fetch(fetchRequest)
+            
+        } catch let error as NSError{
+            print(error.localizedDescription)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Полное удаление
+//        let context = getContext()
+//        let fetchRequest: NSFetchRequest<LinkItem> = LinkItem.fetchRequest()
+//        if let results = try? context.fetch(fetchRequest){
+//            for object in results {
+//                context.delete(object)
+//            }
+//        }
+//
+//        do {
+//            try context.save()
+//
+//        } catch let error as NSError{
+//            print(error.localizedDescription)
+//        }
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -39,8 +114,7 @@ extension AllLinksViewController: UITableViewDelegate, UITableViewDataSource {
         
         let index = linkItems[indexPath.row]
         
-        cell.title.text = index.title
-        cell.urlLink.text = index.link
+        cell.urlLink.text = index.urlLink
         
         return cell
     }
